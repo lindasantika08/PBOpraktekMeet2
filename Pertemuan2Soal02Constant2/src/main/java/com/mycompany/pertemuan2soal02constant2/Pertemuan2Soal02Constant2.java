9f82e31154e92aa5dbad9a50a0d5dcdc1f8f018d
lindasantika08 <123919343+lindasantika08@users.noreply.github.com>
2023-09-01 21:37:20 +0700

    Update Pertemuan2Soal02Constant2.java

--- Pertemuan2Soal02Constant2/src/main/java/com/mycompany/pertemuan2soal02constant2/Pertemuan2Soal02Constant2.java
+++ Pertemuan2Soal02Constant2/src/main/java/com/mycompany/pertemuan2soal02constant2/Pertemuan2Soal02Constant2.java
@@ -10,10 +10,11 @@ package com.mycompany.pertemuan2soal02constant2;
  */
 public class Pertemuan2Soal02Constant2 {
 
-    public static final double CM_PER_INCH = 2.54;
+    public static final double CM_PER_INCH = 2.54; // Konstanta untuk mengonversi inci ke sentimeter
     public static void main(String[] args) {
-        double paperWidth = 8.5;
-        double paperHeight = 11;
-        System.out.println("Paper size in centimeters: " + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+        double paperWidth = 8.5;  // Deklarasi variabel lebar kertas dalam inci
+        double paperHeight = 11;  // Deklarasi variabel tinggi kertas dalam inci
+        System.out.println("Paper size in centimeters: " + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);  // Menghitung dan mencetak ukuran kertas dalam sentimeter
         }
 }
+
