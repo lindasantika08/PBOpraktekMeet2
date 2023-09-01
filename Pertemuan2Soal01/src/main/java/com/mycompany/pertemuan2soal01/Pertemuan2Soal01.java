20e316d34f57473d953515db2b14650020b025e4
lindasantika08 <123919343+lindasantika08@users.noreply.github.com>
2023-09-01 21:39:23 +0700

    Update Pertemuan2Soal01.java

--- Pertemuan2Soal01/src/main/java/com/mycompany/pertemuan2soal01/Pertemuan2Soal01.java
+++ Pertemuan2Soal01/src/main/java/com/mycompany/pertemuan2soal01/Pertemuan2Soal01.java
@@ -14,15 +14,16 @@ import java.util.Scanner;
 public class Pertemuan2Soal01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt(); // Jumlah test case
-        
+        int t = scanner.nextInt(); // Membaca umlah test case dari input
+
+        // loop untuksetiap test case
         for (int i = 0; i < t; i++) {
             try {
                 long n = scanner.nextLong(); // Nilai input
                 
                 System.out.println(n + " can be fitted in:");
                 
-                // Pengecekan tipe data
+                // Pengecekan tipe data yang cocok untuk nilai 'n'
                 if (n >= Byte.MIN_VALUE && n <= Byte.MAX_VALUE) {
                     System.out.println("* byte");
                 }
@@ -36,6 +37,7 @@ public class Pertemuan2Soal01 {
                     System.out.println("* long");
                 }
             } catch (Exception e) {
+                // Tangkap kesalahan jika terjadi dan beri pesan
                 System.out.println(scanner.next() + " can't be fitted anywhere.");
             }
         }
