0b4b5a1c2902d8c1c819ce7473abcc75bba87b05
lindasantika08 <123919343+lindasantika08@users.noreply.github.com>
2023-09-01 21:33:34 +0700

    Update Pertemuan2Soal02Constants.java

--- Pertemuan2Soal02Constants/src/main/java/com/mycompany/pertemuan2soal02constants/Pertemuan2Soal02Constants.java
+++ Pertemuan2Soal02Constants/src/main/java/com/mycompany/pertemuan2soal02constants/Pertemuan2Soal02Constants.java
@@ -10,10 +10,11 @@ package com.mycompany.pertemuan2soal02constants;
  */
 public class Pertemuan2Soal02Constants {
     public static void main(String[] args) {
-    final double CM_PER_INCH = 2.54;
-    double paperWidth = 8.5;
-    double paperHeight = 11;
+    final double CM_PER_INCH = 2.54;   // Deklarasi dan inisialisasi konstanta CM_PER_INCH dengan nilai 2.54
+    double paperWidth = 8.5;  // Deklarasi dan inisialisasi variabel paperWidth dengan nilai 8.5
+    double paperHeight = 11;  // Deklarasi dan inisialisasi variabel paperHeight dengan nilai 11
+
+    // Menghitung dan mencetak ukuran kertas dalam sentimeter dengan mengalikan lebar dan tinggi kertas dengan CM_PER_INCH
     System.out.println("Paper size in centimeters: " + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
    } 
 }
-
