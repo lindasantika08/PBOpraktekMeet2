db2e799bada24ebeff832180a98fe57478d6a2ea
lindasantika08 <123919343+lindasantika08@users.noreply.github.com>
2023-09-01 21:42:55 +0700

    Update Pertemuan2Soal04ConvertData.java

--- Pertemuan2Soal04ConvertData/src/main/java/com/mycompany/pertemuan2soal04convertdata/Pertemuan2Soal04ConvertData.java
+++ Pertemuan2Soal04ConvertData/src/main/java/com/mycompany/pertemuan2soal04convertdata/Pertemuan2Soal04ConvertData.java
@@ -10,16 +10,17 @@ package com.mycompany.pertemuan2soal04convertdata;
  */
 public class Pertemuan2Soal04ConvertData {
 
+    // Cara untuk mengonversi dari long ke short
     static short methodOne(long l)
     {
-        int i = (int) l;
-        return (short)i;
+        int i = (int) l;  // Mengonversi dari long ke int
+        return (short)i;  // Mengonversi dari int ke short
     }
     public static void main(String[] args)
     {
-        double d = 10.25;
-        float f = (float) d;
-        byte b = (byte) methodOne((long) f);
-        System.out.println(b);
+        double d = 10.25; // Mendeklarasikan variabel d bertipe double dengan nilai 10.25
+        float f = (float) d;  // Mengonversi dari double ke float
+        byte b = (byte) methodOne((long) f); // Mengonversi dari float ke long melalui metode, lalu dari long ke byte
+        System.out.println(b); // Mencetak nilai b ke layar
     }
 }
